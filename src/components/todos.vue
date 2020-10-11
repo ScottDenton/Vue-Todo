f0c5b26bc6e8ecc1230c182e35a10e24962af9f6
ScottDenton <scott.denton.sde@gmail.com>
2020-10-11 09:36:26 -0700

    added basic js functionality to todos

---
+++ src/components/todos.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Our todo list</h1>
+    <h2>Enter your next todo</h2>
+    <form>
+      <input id='todoInput' type="text">
+      <button type='submit' @click.prevent='addTodo'>Add</button>
+    </form>
+
+    <hr>
+    <h2>Here are all my TODO's</h2>
+    <li
+      v-for='(todo,ind) in todos'
+      :key=ind> {{ todo }}
+    </li>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    data() {
+      return {
+          todos: [],
+      }
+    },
+
+    methods: {
+      getTodo() {
+        return document.getElementById('todoInput').value
+      },
+
+      clearInput() {
+        return document.getElementById('todoInput').value = ''
+      },
+
+      addTodo() {
+        this.todos.push(this.getTodo())
+        this.clearInput();
+      }
+    }
+
+  }
+</script>
