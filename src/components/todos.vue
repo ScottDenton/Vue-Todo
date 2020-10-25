3b496d3e362f3c1741fc078212991ac7e645621f
JennaTranter <jenna.tranter@gmail.com>
2020-10-25 13:59:47 -0700

    fixed bootstrap and updated input styling

--- src/components/todos.vue
+++ src/components/todos.vue
@@ -3,8 +3,12 @@
     <h1>Our todo list</h1>
     <h2>Enter your next todo</h2>
     <form>
-      <input id='todoInput' type="text">
-      <button type='submit' @click.prevent='addTodo'>Add</button>
+    <div class="input-group mb-3">
+      <input id='todoInput' type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click.prevent='addTodo' >Add</button>
+      </div>
+    </div>
     </form>
 
     <hr>
